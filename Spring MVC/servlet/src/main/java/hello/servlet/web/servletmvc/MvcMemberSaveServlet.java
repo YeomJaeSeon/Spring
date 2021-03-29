5b51cf584d74a119f58762787eacb4fa67cb230f
YeomJaeSeon <a89541457@gmail.com>
2021-03-29 14:03:42 +0900

    jsp servlet만으로 MVC적용하기, 뷰는 무적건 컨트롤러를 거쳐서 가야함. 모델은 데이터를 담고 뷰에서 모델을통해서 데이터참조해 화면을 랜더링함. 이는 비즈니스로직 컨트롤러(서비스 리파지토리)와 뷰를 완전히 분업화하게 도와줌. /WEB-INF를 통해서 jsp(뷰)에 url로 웹브라웆어서 바로요청을 막음. 이는 MVC패턴의 컨트롤러를 통해서만 뷰를호출할수있게하는 약속임

---
+++ Spring MVC/servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberSaveServlet.java
@@ -0,0 +1,37 @@
+package hello.servlet.web.servletmvc;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
+public class MvcMemberSaveServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        //Model에 데이터를 보관해야함. 뷰가 접근할수있는 모델.
+        request.setAttribute("member", member);
+
+        //컨트롤러를거친후 view호출
+        String viewPath = "/WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+
+    }
+}
