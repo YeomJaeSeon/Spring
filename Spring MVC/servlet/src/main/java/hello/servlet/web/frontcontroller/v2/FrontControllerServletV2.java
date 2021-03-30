8ff9d824449cbf12f80561b422f9f0c50cbb4ab3
YeomJaeSeon <a89541457@gmail.com>
2021-03-30 14:45:55 +0900

    각 컨트롤러에서 JSP 포워딩 하는 부분의 코드 중복을 MyView의 클래스 추가로 해결. 각 컨트롤러는 MYView인스턴스를 리턴하고 프론트컨트롤러에서 해당 인스턴스의 메서드인 render함수를 호출해서 각 JSP로 포워딩후 해당 JSP 응답

---
+++ Spring MVC/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -0,0 +1,47 @@
+package hello.servlet.web.frontcontroller.v2;
+
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+// v1하위에들어오면 어떻게들어오던 (자식의 자식으로들어오던)무조건 이 서블릿 호출됨.
+@WebServlet(name = "frontControllerServletV2", urlPatterns = "/front-controller/v2/*")
+public class FrontControllerServletV2 extends HttpServlet {
+
+    //컨트롤러 매핑정보
+    private Map<String, ControllerV2> controllerV1Map = new HashMap<>();
+
+    //생성자에서 컨트롤러 매핑정보 생성
+    public FrontControllerServletV2() {
+        controllerV1Map.put("/front-controller/v2/members/new-form", new MemberFormControllerV2());
+        controllerV1Map.put("/front-controller/v2/members/save", new MemberSaveControllerV2());
+        controllerV1Map.put("/front-controller/v2/members", new MemberListControllerV2());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+
+        // 부모클래스타입의 참조변수가 자식클래스의인스턴스 타입 참조가능. - 다형성
+        // == ControllerV1 controller = new MemberListControllerV1()이거랑 똑같음.
+        ControllerV2 controller = controllerV1Map.get(requestURI); // 인터페이스로꺼내게됨.
+        if(controller == null){ // 예외처리
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // 다형서응로 오버라이딩된 메서드를 호출할것이다.
+        MyView view = controller.process(request, response);
+        view.render(request, response);
+    }
+}
