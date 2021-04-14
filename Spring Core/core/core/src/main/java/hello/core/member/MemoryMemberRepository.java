45dd746ce67ea24b9ecb40e0080f5dcd663d87bd
YeomJaeSeon <a89541457@gmail.com>
2021-04-14 10:51:22 +0900

    회원 도메인 개발

---
+++ Spring Core/core/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 인터페이스를 통해서 역할 구현, 선언 구현, 껍데기 알맹이로분리해서 이 구현부분은 슉슉 갈아끼우기 쉬워짐.
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
