faa17efddc3ee34bb598b4e8d1dd63228bd59ef8
YeomJaeSeon <a89541457@gmail.com>
2021-04-14 11:05:46 +0900

    회원도메인 테스트 - 간단함

---
+++ Spring Core/core/core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+// junit안쓰고 테스트하는거임.
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl(); // 다형성
+        Member memberA = new Member(1L, "memberA", Grade.BASIC);
+        memberService.join(memberA);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("memberA = " + memberA.getName());
+        System.out.println("findMember = " + findMember.getName());
+        System.out.println(memberA == findMember);
+    }
+}
--- Spring Core/core/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ Spring Core/core/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,7 @@
 package hello.core.member;
 
-// 관례적으로 인터페이스에대한 구현체가 하나면 이름을 Impl을 뒤에붙임.
 public class MemberServiceImpl implements MemberService{
 
-    // 다형성 - 부모타입의 참조변수로 자식인스턴스접근. 인터페이스와 다형성으로 역할 구현분리
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
