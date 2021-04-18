3672730e62e0a4a7e7e26991345de2deb511e65f
YeomJaeSeon <a89541457@gmail.com>
2021-04-18 17:14:07 +0900

    캐시의 기본동작.. 만약 캐시 유효기간이 끝났어도 리소스의 변겨이없다면 과연..?

--- Spring Core/core/core/src/main/java/hello/core/MemberApp.java
+++ Spring Core/core/core/src/main/java/hello/core/MemberApp.java
@@ -13,9 +13,11 @@ public class MemberApp {
 //        AppConfig appConfig = new AppConfig();
 //        MemberService memberService = appConfig.memberService();
 
-        // 이게 Spring컨테이너임
+        // 이게 Spring컨테이너임 - 인터페이스임                   해당인터페이스 구현한 구체 클래스임.     요로케 스프링 컨테이너생성할때 설정 정보를 같이전달한다.
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
+        // 참고로 스프링 컨테이너 생성할때 설정정보는 자바 클래스파일또는 XMl로 만들수있다.
 
 
         Member memberA = new Member(1L, "memberA", Grade.BASIC);
