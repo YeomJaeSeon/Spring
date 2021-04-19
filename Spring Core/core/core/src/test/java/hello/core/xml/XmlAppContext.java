6be1b3663ed3f0e47ecb109dcaeff0e48aef63fe
YeomJaeSeon <a89541457@gmail.com>
2021-04-19 15:40:23 +0900

    XML로 스프링 컨테이너 설정하기

---
+++ Spring Core/core/core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,17 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+//XML로 스프링 컨테이너 설정가능 (지금까지한건 자바 클래스로 했음.)
+public class XmlAppContext {
+    @Test
+    void XmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
