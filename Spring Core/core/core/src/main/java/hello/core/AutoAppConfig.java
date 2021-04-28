c2388ff12d08ae4adde29632103161b38d09a849
YeomJaeSeon <a89541457@gmail.com>
2021-04-28 16:06:07 +0900

    스프링빈 이름이 같아서 충돌일어날경우.. 자동vs자동 무조건 예외발생, 그러나 수동 vs자동은 수동으로 등록된 스프링빈이 우선권을 갖는데 이런건 중요하지않고 무조건 스프링빈간의 이름을 같게하여 충돌이 일어나지 않도록 하는 것이중요하다. 명확하지 않는 부분은 애초에 배제하도록!

--- Spring Core/core/core/src/main/java/hello/core/AutoAppConfig.java
+++ Spring Core/core/core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,4 +13,10 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+    // 자동으로 등록되어있는 스프링빈과 이름이같으면 수동으로 등록된 스프링빈이 우선권을 갖긴한데 그냥 무조건 이름 다르게해야한다생각하자.
+    // 이러면 에러잡기가 너무어려움.. 그래서 스프링부트도 최신엔 자동등록 스프링빈과 수동등록 스프링빈간의 이름 충돌을 에러로 바꿔버림
+//    @Bean("memoryMemberRepository") // @Component붙은 클래스 맨앞 글자 소문자로 바뀌어서 스프링 빈 이름으로 등록됨.
+//    MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
 }
